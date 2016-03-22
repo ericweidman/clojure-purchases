a1ecf8f6f87b8957bd13e911cb19c433f784b458
ericweidman <ericweidman@gmail.com>
2016-03-22 13:36:15 -0400

    Added parsing saving

--- src/clojure_purchases/core.clj
+++ src/clojure_purchases/core.clj
@@ -1,5 +1,23 @@
 (ns clojure-purchases.core
+  (:require [clojure.string :as str]
+            [clojure.walk :as walk])
   (:gen-class))
+  
 
 (defn -main []
-  (println "Hello, World!"))
+  (let [purchase (slurp "purchases.csv")
+        purchase (str/split-lines purchase)
+        purchase (map (fn [line]
+                       (str/split line #","))
+                     purchase)
+        header (first purchase)
+        purchase (rest purchase)
+        purchase (map (fn [line]
+                        (apply hash-map (interleave header line)))
+                   purchase)
+        purchase (walk/keywordize-keys purchase)
+        purchase (filter (fn [line]
+                           (=(:category line) "Furniture"))
+                   purchase)]
+    (spit "filtered_purchase.edn" (pr-str purchase))
+    purchase))
