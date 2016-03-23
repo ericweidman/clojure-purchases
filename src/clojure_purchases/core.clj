2f89cb862eb872acdb31ec1ce98de13e1954013c
ericweidman <ericweidman@gmail.com>
2016-03-23 13:39:21 -0400

    Added website stuff

--- src/clojure_purchases/core.clj
+++ src/clojure_purchases/core.clj
@@ -1,14 +1,15 @@
 (ns clojure-purchases.core
   (:require [clojure.string :as str]
-            [clojure.walk :as walk])
+            [clojure.walk :as walk]
+            [compojure.core :as c]
+            [ring.adapter.jetty :as j]
+            [ring.middleware.params :as p]
+            [hiccup.core :as h])
   (:gen-class))
   
 
-(defn -main []
-  (println "Please type a category")
-  (println "i.e. Alcohol, Furniture, Food, Shoes, Toiletries")
+(defn read-purchase []
   (let [purchase (slurp "purchases.csv")
-        text (read-line)
         purchase (str/split-lines purchase)
         purchase (map (fn [line]
                        (str/split line #","))
@@ -18,12 +19,43 @@
         purchase (map (fn [line]
                         (apply hash-map (interleave header line)))
                    purchase)
-        purchase (walk/keywordize-keys purchase)
-        purchase (filter (fn [line]
-                           (=(:category line) text))
-                         purchase)]
-    (spit "filtered_purchase.edn" (pr-str purchase))
+        purchase (walk/keywordize-keys purchase)]
+    ;(spit "filtered_purchase.edn" (pr-str purchase))
     purchase))
     
+(defn category-html [purchase]
+    (let [all-category (map :category purchase)
+           unique-category (set all-category)
+           sorted-category (sort unique-category)]
+     [:div
+      (map (fn [category]
+            [:span
+             [:a {:href (str "/?category=" category)} category]
+             " "])
+         sorted-category)]))
 
+(defn purchase-html [purchase]
+  [:ul
+   (map (fn [purchase]
+          [:li (str (:customer_id purchase) " " (:date purchase) " " (:credit_card purchase) " " (:cvv purchase))])
+        purchase)])
+
+
+(c/defroutes app
+  (c/GET "/" request
+    (let [params (:params request)
+          category (get params "category")
+          category (or category "Food")
+          purchase (read-purchase)
+          filtered-purchase (filter (fn [purchase]
+                                     (= (:category purchase) category))
+                              purchase)]
+     (h/html [:html
+              [:body 
+               (category-html purchase)
+               (purchase-html filtered-purchase)]]))))
+
+
+(defn -main []
+  (j/run-jetty (p/wrap-params app) {:port 3000}))
    
